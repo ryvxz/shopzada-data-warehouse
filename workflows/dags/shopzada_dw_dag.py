2ee2a5a4f90deb701db2f886c64b0259aefbd898
Ken <keennnfajardo45@gmail.com>
2025-12-04 15:17:44 +0800

    Restructured to initial prototype Main DAG

--- workflows/dags/shopzada_dw_dag.py
+++ workflows/dags/shopzada_dw_dag.py
@@ -10,12 +10,18 @@ from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.sdk import chain, DAG, TaskGroup
 
-def run_script(script_folder: str, script_name: str, dag_folder: str):
+
+folder = "/opt/airflow/plugins/scripts"
+
+def run_script(script_folder: str, script_name: str):
     """A callable to run scripts from a specified script folder."""
-    scripts_path = Path(dag_folder).joinpath('..', '..', 'scripts', script_folder)
+    scripts_path = Path(folder).joinpath(script_folder)
     sys.path.insert(0, str(scripts_path))
-    module = import_module(script_name)
-    module.main() # Assuming each script has a main() function
+    try:
+        module = import_module(script_name)
+        module.main()  # Assuming each script has a main() function
+    finally:
+        sys.path.pop(0)
 
 with DAG(
     dag_id='shopzada_data_warehouse',
@@ -30,28 +36,30 @@ with DAG(
         ingest_all_sources = BashOperator(
             task_id='ingest_all_sources',
             bash_command='python ingest.py',
-            cwd='{{ dag_folder }}/../../scripts/ingestion'
+            cwd=f'{folder}/ingestion',
+            retries=3
         )
 
         load_to_staging_db = PythonOperator(
             task_id='load_to_staging_db',
             python_callable=run_script,
-            op_kwargs={'script_folder': 'ingestion', 'script_name': 'ingest_to_sql', 'dag_folder': '{{ dag_folder }}'},
+            op_kwargs={'script_folder': 'ingestion', 'script_name': 'ingest_to_sql'},
+            retries=3
         )
         ingest_all_sources >> load_to_staging_db
+
 
     with TaskGroup('transform_and_quality_checks', tooltip='Transform data and perform quality checks') as transform_and_quality_checks:
         transform_data = PythonOperator(
             task_id='transform_data',
             python_callable=run_script,
-            op_kwargs={'script_folder': 'transformation', 'script_name': 'transform_data', 'dag_folder': '{{ dag_folder }}'},
+            op_kwargs={'script_folder': 'transformation', 'script_name': 'transform_data'},
         )
         quality_checks = PythonOperator(
             task_id='quality_checks',
             python_callable=run_script,
-            op_kwargs={'script_folder': 'transformation', 'script_name': 'quality_checks', 'dag_folder': '{{ dag_folder }}'},
+            op_kwargs={'script_folder': 'transformation', 'script_name': 'quality_checks'},
         )
-        #default dependency transform >> quality
 
     with TaskGroup('load_to_dw', tooltip='Load data into the data warehouse') as load_to_dw:
         load_physical_model = EmptyOperator(task_id='load_physical_model')
